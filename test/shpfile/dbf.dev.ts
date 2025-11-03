6eaf7b33541ee4f703e7185a101d2bcdea4905eb
unknown <cocaine_coder>
2025-11-03 22:16:11 +0800

    完成dbf读取

--- test/shpfile/dbf.dev.ts
+++ test/shpfile/dbf.dev.ts
@@ -2,4 +2,6 @@ import * as fs from 'fs';
 import { readDbf } from '../../lib/shpfile/dbf';
 
 const file = fs.readFileSync("../mock/shp/点/点.dbf");
-readDbf(file);
+const records = readDbf({file});
+
+console.log(records);
