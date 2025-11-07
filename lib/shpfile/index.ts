1aa6e585e518517119b1bf95f07a8a9c62dc6a25
cocaine_coder <tracywang1997@outlook.com>
2025-11-07 13:28:53 +0800

    update

--- lib/shpfile/index.ts
+++ lib/shpfile/index.ts
@@ -1,1 +1,5 @@
 import { readShp, writeShp } from './shp';
+import { readDbf, writeDbf } from './dbf';
+
+
+export { readShp, writeShp, readDbf, writeDbf };
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,27 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [dts({rollupTypes: true})],
+  plugins: [dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: "./lib/index.ts",
       name: "GJCV",
       fileName: "index",
     },
+    // rollupOptions: {
+    //   external: (id) => {
+    //     // 排除整个 test 目录（相对项目根目录）
+    //     if (id.includes('/test/') || id.includes('\\test\\')) return true
+    //     // 如需连 __tests__ 也一并排除
+    //     if (id.includes('/__tests__/') || id.includes('\\__tests__\\')) return true
+    //     return false
+    //   },
+    //   output: {
+    //     // 可选：避免将空模块写入 chunk，减少产物体积
+    //     manualChunks(id) {
+    //       if (id.includes('/test/') || id.includes('\\test\\')) return null
+    //     },
+    //   },
+    // }
   },
 });
