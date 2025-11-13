af6acc919f9befab0251f6335a35a0ad0e31bcda
cocaine_coder <tracywang1997@outlook.com>
2025-11-13 17:13:36 +0800

    update

--- lib/shpfile/index.ts
+++ lib/shpfile/index.ts
@@ -2,13 +2,22 @@ import { readShp, writeShp } from './shp';
 import { readDbf, writeDbf } from './dbf';
 import { groupBy, TFileLike, transformCoorinates } from '../utils';
 import proj4 from 'proj4';
+import * as zip from '@zip.js/zip.js';
 
-export function readShpFile(options: {
+type TReadOptions = {
     shp: TFileLike,
     dbf: TFileLike,
     encoding?: string,
     crs?: string
-}) {
+}
+
+type TWriteOptions = {
+    data: GeoJSON.FeatureCollection | Array<GeoJSON.Feature>,
+    encoding?: string,
+    crs?: string
+}
+
+export function readShpFile(options: TReadOptions) {
     const shpData = readShp({ file: options.shp });
     const dbfData = readDbf({ file: options.dbf, encoding: options.encoding });
 
@@ -32,11 +41,7 @@ export function readShpFile(options: {
     return { type: 'FeatureCollection', features };
 }
 
-export function writeShpFile(options: {
-    data: GeoJSON.FeatureCollection | Array<GeoJSON.Feature>,
-    encoding?: string,
-    crs?: string
-}) {
+export function writeShpFile(options: TWriteOptions) {
     options.encoding ??= "utf-8";
     let data = options.data instanceof Array ? options.data : options.data.features;
     data = data.filter(x => {
@@ -87,3 +92,39 @@ export function writeShpFile(options: {
 }
 
 export { readShp, writeShp, readDbf, writeDbf };
+
+
+export async function readFromZip(file: Blob | ArrayBuffer) {
+    if (file instanceof ArrayBuffer) {
+        file = new Blob([file], { type: 'application/zip' });
+    }
+
+    const zipReader = new zip.ZipReader(new zip.BlobReader(file));
+
+    const entries = await zipReader.getEntries();
+
+    entries.forEach(entry=>{
+        
+    });
+}
+
+export async function writeToZip(options: TWriteOptions) {
+    const temp = writeShpFile(options);
+
+    const blobWriter = new zip.BlobWriter("application/zip");
+    const zipWriter = new zip.ZipWriter(blobWriter);
+
+    const tasks = temp.reduce((p, x) => {
+        for (const [extensions, buffers] of Object.entries(x.data)) {
+            const blobReader = new zip.Uint8ArrayReader(new Uint8Array(buffers));
+            p.push(zipWriter.add(x.name + "." + extensions, blobReader));
+        }
+
+        return p;
+    }, new Array<Promise<any>>());
+
+    await Promise.all(tasks);
+    await zipWriter.close();
+
+    return await blobWriter.getData();
+}
